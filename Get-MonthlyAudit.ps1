ce2546a7d278c776253a737318a66263731bdba0
bhahn <bhahnnj@gmail.com>
2022-10-11 21:31:59 -0400

    made it better

--- Get-MonthlyAudit.ps1
+++ Get-MonthlyAudit.ps1
@@ -9,9 +9,9 @@ function Get-MonthlyAudit {
         Get-ADGroupMember -Identity $group | select-object Name
         }
         # Select path to save
-        if ($groupname -eq 'USB Allowed' -or 'USB') {
+        if ($groupname -eq 'USB Allowed' -or 'USB' -or '8==D~~~~(())') {
             Export-Csv -Path "\\landolakez\shared\compliance\internal audits\Usb Access\_$((Get-Date).ToShortDateString().Replace("/","-")).csv" -NoTypeInformation -Append
-        elseif ($groupname -eq 'Help-Desk' -or 'Local Admin') {
+        elseif ($groupname -eq 'Help-Derp' -or 'Local Asshole') {
             Export-Csv -Path "\\landolakez\shared\compliance\internal audits\Local Admin\LOCALADMIN_$((Get-Date).ToShortDateString().Replace("/","-")).csv" -NoTypeInformation -Append
 
         }
