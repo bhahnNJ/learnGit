a3c8e2962c666ed05a5ba7f5cc0d719d8ebd3b64
bhahn <bhahnnj@gmail.com>
2022-10-11 21:30:20 -0400

    Initial commit with file.

---
+++ Get-MonthlyAudit.ps1
@@ -0,0 +1,19 @@
+function Get-MonthlyAudit {
+    Param(
+        [string[]]$Groupname,
+        [string]$Path,
+        [string]$email
+    )
+    foreach ($group in $groupname) {
+        # Convert get-adgroupmember results
+        Get-ADGroupMember -Identity $group | select-object Name
+        }
+        # Select path to save
+        if ($groupname -eq 'USB Allowed' -or 'USB') {
+            Export-Csv -Path "\\landolakez\shared\compliance\internal audits\Usb Access\_$((Get-Date).ToShortDateString().Replace("/","-")).csv" -NoTypeInformation -Append
+        elseif ($groupname -eq 'Help-Desk' -or 'Local Admin') {
+            Export-Csv -Path "\\landolakez\shared\compliance\internal audits\Local Admin\LOCALADMIN_$((Get-Date).ToShortDateString().Replace("/","-")).csv" -NoTypeInformation -Append
+
+        }
+        } #foreach
+} #function
